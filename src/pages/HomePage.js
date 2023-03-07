14abf19428e6e9fb5a0c55cee683b187d0a24f49
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-03-07 17:38:53 -0300

    feat: changing common folder to components and small fixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 //import styled from "styled-components";
-import { BallTriangle as LoadingAnimation } from "react-loader-spinner";
+//import { BallTriangle as LoadingAnimation } from "react-loader-spinner";
 import Navbar from "../common/Navbar";
 
 export default function HomePage(){
