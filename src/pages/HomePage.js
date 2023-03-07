6b192a442ae00d9276c37493e5eabbfda5a0a250
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-03-07 18:19:52 -0300

    fix: adding .js extension to import

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 //import styled from "styled-components";
 //import { BallTriangle as LoadingAnimation } from "react-loader-spinner";
-import Navbar from "../common/Navbar";
+import Navbar from "../components/Navbar.js";
 
 export default function HomePage(){
     return (
