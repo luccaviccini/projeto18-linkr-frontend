5742a3555a1e47330a96e46a70b6f6736dea67c3
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-03-07 12:48:15 -0300

    feat: App.js gets  structure for routes, including home page route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  UserContext  from "./context/UserContext";
+import Home from "./pages/HomePage";
 
 
 
 function App() {
   return (
-    <h1>
-      Projeto Linkr
-    </h1>
+    <UserContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+
     
   );
 }
