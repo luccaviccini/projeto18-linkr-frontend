c09c4f17fc33ddc46a995d44a8b7769bb60f4369
theomelgar <theomelgar@gmail.com>
2023-03-09 00:55:28 -0300

    feat/style: Posts example

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,37 @@
 import styled from "styled-components";
+import { AiOutlineHeart } from 'react-icons/ai'
 
 export default function Post() {
+
   return (
     <PostContainer>
-      <UserImg src="https://www.w3schools.com/howto/img_avatar.png" />
-        <PostTitle>Post Title</PostTitle>
+      <InfoSection>
+        <UserImg src="https://www.w3schools.com/howto/img_avatar.png" />
+        <Likes>
+          <AiOutlineHeart />
+          1000
+        </Likes>
+      </InfoSection>
+      <ContentSection>
+        <Author>Autor</Author>
+        <Description>Olha que legal</Description>
+        <Content>
+          <TextContent>
+            <PostTitle>
+              Title
+            </PostTitle>
+            <PostSummary>
+              TEXT TEXT TEXT TEXT TEXT TEXT
+            </PostSummary>
+            <PostLink>
+              https://www.youtube.com/watch?v=dQw4w9WgXcQ
+            </PostLink>
+          </TextContent>
+          <LinkImage>
+            <img src="https://www.w3schools.com/howto/img_avatar.png" />
+          </LinkImage>
+        </Content>
+      </ContentSection>
     </PostContainer>
   );
 }
@@ -17,25 +44,121 @@ const PostContainer = styled.div`
   background: #171717;
   border-radius: 16px;
   margin: 29px 0;
-
+  display: flex;
+  justify-content: space-around;
   @media (max-width: 937px) {
     border-radius: 0;
+    justify-content: center;
   }
 `;
 
+const ContentSection = styled.div`
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+const LinkImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  img{
+    width: 153.44px;
+    height: 155px;
+    background: url(image.png);
+    border-radius: 0px 12px 13px 0px;
+  }
+`
+
+const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+const InfoSection = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+  `
 const UserImg = styled.img`
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  margin: 16px 18px;
+  margin: 16px 15px;
 `;
 
-const PostTitle = styled.h1`
+
+const Author = styled.div`
   font-family: "Lato";
   font-style: normal;
   font-weight: 300;
   font-size: 20px;
   line-height: 24px;
 
-  color: #707070;
+  color: #fff;
+`;
+const Likes = styled.div`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin:10px;
+  color: #ffffff;
+`;
+const Description = styled.div`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+
+  color: #B7B7B7;
+`;
+const Content = styled.div`
+  display: flex;
+  max-width: 503px;
+  max-height: 155px;
+  width: 100%;
+  height: 100%;
+  background: #171717;
+  border: 1px solid #4D4D4D;
+  border-radius: 11px;
+
+  @media (max-width: 937px) {
+    border-radius: 0;
+  }
+`;
+
+const PostTitle = styled.h1`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  font-size: 16px;
+
+  color: #CECECE;
+`;
+
+const PostSummary = styled.div`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+  font-size: 11px;
+  color: #9B9595;
+`;
+const PostLink = styled.div`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+  font-size: 11px;
+  color: #CECECE;
 `;
