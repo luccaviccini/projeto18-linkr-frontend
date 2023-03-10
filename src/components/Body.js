bee5ff0a6d76d326f0520fa1bf5ee465c30352b8
theomelgar <theomelgar@gmail.com>
2023-03-10 19:17:34 -0300

    feat: loading and no posts notify

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,10 +5,12 @@ import Trending from "./Trending.js"
 import { useEffect, useState, useContext } from "react";
 import UserContext from "../context/UserContext.js";
 import axios from "axios";
+import Loading from "./Loading.js";
 
 export default function Body() {
   const { userData } = useContext(UserContext);
   const [posts, setPosts] = useState([]);
+  const [ loading, setLoading ] = useState(true)
 
   useEffect(() => {
     // get token from local storage
@@ -23,8 +25,8 @@ export default function Body() {
     axios
       .get(`${process.env.REACT_APP_API_URL}/timeline`, createConfig(userData.token))
       .then((response) => {
-        console.log(response.data);
         setPosts(response.data);
+        setLoading(false)
       })
       .catch((error) => {
         console.log(error);
@@ -37,28 +39,26 @@ export default function Body() {
     <BodyContainer>
       <Left>
         <Title>timeline</Title>
-        <NewPost/>   
-        {posts.map((post) => (
-          <Post
-            key={post.id}            
-            username={post.author}            
-            siteUrl={post.siteUrl}
-            title={post.title}
-            description={post.description}
-            userImg={post.pictureUrl}
-            imageUrl={post.imageUrl}            
-            likes={post.likes} 
-            lastTwoUsersLiked={post.users}   
-            metaDescription={post.metaDescription}        
-          />
-        ))}
-
-        
-
-
-
-        
-         
+        <NewPost />
+        {!posts ? <h3>"There are no posts yet"</h3> : 
+        posts.length > 0 ?
+          posts.map((post) => (
+            <Post
+              key={post.id}
+              id={post.id}
+              username={post.author}
+              siteUrl={post.siteUrl}
+              title={post.title}
+              description={post.description}
+              userImg={post.pictureUrl}
+              imageUrl={post.imageUrl}
+              likes={post.likes}
+              lastTwoUsersLiked={post.users}
+              metaDescription={post.metaDescription}
+            />
+          )) :
+          <Loading/>
+        }
         
       </Left>
       <Trending />
@@ -98,4 +98,13 @@ const BodyContainer = styled.div`
 const Left = styled.div`
   width: 100%;
   max-width: 611px; 
+  h3{
+    color: white;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+    margin: 20px;
+  }
 `;
