8245bc23ed5f0e9b08545c0ae54f3694c712a62e
lucasmartins-dev <lucasmartinsms.dev@gmail.com>
2023-03-25 18:41:59 -0300

    edit mode

---
+++ src/context/PostsProvider.js
@@ -0,0 +1,32 @@
+
+
+import { createContext, useState } from "react";
+
+ const PostsContext = createContext();
+
+export function PostsProvider({ children }) {
+
+    const [posts, setPosts] = useState([])
+    const [updatedPosts, setUpdatedPosts] = useState([])
+    const [postsToUpdate, setPostsToUpdate] = useState(0)
+    const [mustUpdatePosts, setMustUpdatePosts] = useState(false)
+    const [sendPost, setSendPost] = useState(false)
+    const [gotPosts, setGotPosts] = useState(false)
+
+    return (
+        <PostsContext.Provider
+            value={{
+                posts, setPosts,
+                updatedPosts, setUpdatedPosts,
+                postsToUpdate, setPostsToUpdate,
+                mustUpdatePosts, setMustUpdatePosts,
+                sendPost, setSendPost,
+                gotPosts, setGotPosts
+            }}
+        >
+            {children}
+        </PostsContext.Provider>
+    )
+}
+
+export default PostsContext
